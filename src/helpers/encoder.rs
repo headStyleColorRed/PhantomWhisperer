bae0ee958596713ed9b13aa83280cb1e96974c80
Rodrigo Labrador <headstylecolorred@gmail.com>
2024-07-01 18:13:20 +0200

    updated gitignore

--- src/helpers/encoder.rs
+++ src/helpers/encoder.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::Write;
 
 pub fn encode_message(message: &str, output_file: &str) -> Result<(), std::io::Error> {
-    println!("Encoding message: {}", message);
+    println!("[ENCODER]: Encoding message");
 
     // Convert message to bytes
     let message_bytes = message.as_bytes();
@@ -11,12 +11,12 @@ pub fn encode_message(message: &str, output_file: &str) -> Result<(), std::io::E
     // Encode to Base64
     let encoded = general_purpose::STANDARD.encode(message_bytes);
 
-    println!("Encoded message: {}", encoded);
+    println!("[ENCODER]: Message encoded");
 
     // Save encoded message to a file
     let mut file = File::create(output_file)?;
     file.write_all(encoded.as_bytes())?;
 
-    println!("Encoded message saved to: {}", output_file);
+    println!("[ENCODER]: Encoded message written to file: {}", output_file);
     Ok(())
 }
